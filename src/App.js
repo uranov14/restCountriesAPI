f1085c029303d882d1f3c261cc1fc95aa54bab02
uranov14 <102876402+uranov14@users.noreply.github.com>
2022-10-15 14:55:50 +0300

    Changes App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react"
 import Countries from "./components/Countries"
 import Country from "./components/Country"
-import { BrowserRouter as Router, HashRouter, Routes, Route} from "react-router-dom"
+import { HashRouter, Routes, Route} from "react-router-dom"
 import Header from "./components/Header"
 
 function App() {
   return (
-    <Router>
-      <HashRouter basename="/">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Countries />} />
-          <Route path="/countries/:name" element={<Country />}/>
-        </Routes>
-      </HashRouter> 
-    </Router>
+    <HashRouter basename="/">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Countries />} />
+        <Route path="/countries/:name" element={<Country />}/>
+      </Routes>
+    </HashRouter> 
   );
 }
 
